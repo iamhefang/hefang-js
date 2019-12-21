029784f2748667053de450231dc86ed8c8c3b1be
hefang <he@hefang.link>
2019-12-21 10:30:36 +0800

    添加单元测试

--- test/array.test.js
+++ test/array.test.js
@@ -1,10 +1,11 @@
-import {flatArray, range} from "../src/array";
+const H = require("../index");
 
 test("测试array.tsx", function () {
-    expect(range(1, 5)).toEqual([1, 2, 3, 4, 5]);
-    expect(range(1, 5, 2)).toEqual([1, 3, 5])
+    expect(H.range(1, 5)).toEqual([1, 2, 3, 4, 5]);
+    expect(H.range(1, 5, 2)).toEqual([1, 3, 5])
 });
 
 test("测试数组扁平化", function () {
-    expect(flatArray([1, 2, 3, [4, 5, [6]]])).toEqual([1, 2, 3, 4, 5, 6])
+    expect(H.flatArray([1, 2, 3, [4, 5, [6]]])).toEqual([1, 2, 3, 4, 5, [6]]);
+    expect(H.flatArray([1, 2, 3, [4, 5, [6]]], 2)).toEqual([1, 2, 3, 4, 5, 6])
 });
---
+++ test/object.test.js
@@ -0,0 +1,26 @@
+const H = require("../index");
+
+test("测试object.tsx", function () {
+    expect(H.range(1, 5)).toEqual([1, 2, 3, 4, 5]);
+    expect(H.range(1, 5, 2)).toEqual([1, 3, 5]);
+
+    const obj = {
+        a: {
+            aa: {
+                aaa: 2
+            }
+        },
+        b: {
+            bb: 111
+        },
+        c: {},
+        500: "error"
+    };
+
+    expect(H.chainingCheck(obj, "a.aa.aaa")).toEqual(2);
+    expect(H.chainingCheck(obj, "a", "aa", "aaa")).toEqual(2);
+    expect(H.chainingCheck(obj, "c.cc.ccc")).toBeNull();
+    expect(H.chainingCheck(obj, "c")).toEqual({});
+    expect(H.chainingCheck(obj, "d")).toBeNull();
+    expect(H.chainingCheck(obj, 500)).toEqual("error")
+});
